a11328897382b492b062c660a0b65ac9a5f65b81
Eva Bartolomeu <66091692+eva-pomposo@users.noreply.github.com>
2022-12-29 17:25:14 +0000

    start and finish CSURÖS’ APPROXIMATE COUNTER

---
+++ approximate_counters.py
@@ -0,0 +1,42 @@
+import random
+import string
+import argparse
+
+M = 100
+
+def fp_increment(x):
+    t = x // M
+    while t > 0:
+        if random.getrandbits(1) == 1:
+            return x
+        t -= 1
+    return x + 1
+
+def estimate_frequent_letters(text):
+    # Create a dictionary to store the letter counts
+    letter_counts = {letter: 0 for letter in string.ascii_uppercase}
+
+    # Iterate through each letter in the text and increment the count using fp_increment
+    for letter in text:
+        if letter.isspace():
+            continue
+        letter_counts[letter] = fp_increment(letter_counts[letter])
+
+    # Sort the letters by their estimated count in descending order
+    return dict(sorted(letter_counts.items(), key=lambda item: item[1], reverse=True))
+
+# Parse the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", "-i", help="Path to the input text file", required=True)
+args = parser.parse_args()
+
+# Read the preprocessed text file
+with open(args.input, "r", encoding="utf-8") as f:
+    text = f.read()
+
+# Estimate the most frequent letters using the approximate floating-point counter with m = 100
+frequent_letters = estimate_frequent_letters(text)
+
+# Print the estimated counts of the most frequent letters
+for letter, count in frequent_letters.items():
+    print(f"{letter}: {count}")
