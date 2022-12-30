d46f5cf3c8244577d35bd4bd426a33de22a627cc
Eva Bartolomeu <66091692+eva-pomposo@users.noreply.github.com>
2022-12-30 08:51:55 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 # Run the preprocessing script
 python3 file_processor.py -i literary_works/pg69644.txt -o processed_files/pg69644.txt 
-# Run the exact and approximate counter scripts
+# Run the exact counter
 python3 exact_counters.py -i processed_files/pg69644.txt  
+# Run the approximate counter
 python3 approximate_counters.py -i processed_files/pg69644.txt
+# Run the lossy counter
+python3 lossy_count.py -i processed_files/pg69644.txt
