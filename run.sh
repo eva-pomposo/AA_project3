184ac442b5d39889fa8f0d1c2537c9e1114e1c84
eva-pomposo <evabartolomeu@ua.pt>
2023-01-08 15:00:36 +0000

    script

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Run the preprocessing script
-python3 file_processor.py -i literary_works/pg69644.txt -o processed_files/pg69644.txt 
-# Run the exact counter
-python3 exact_counters.py -i processed_files/pg69644.txt  
-# Run the approximate counter
-python3 approximate_counters.py -i processed_files/pg69644.txt
-# Run the lossy counter
-python3 lossy_count.py -i processed_files/pg69644.txt
+# Run the main.py script for each of the literary works
+python3 main.py -i literary_works/Romeo_and_Juliet_finnish.txt -o processed_files/Romeo_and_Juliet_finnish.txt -r Romeo_and_Juliet_finnish.txt
+python3 main.py -i literary_works/Romeo_and_Juliet_french.txt -o processed_files/Romeo_and_Juliet_french.txt -r Romeo_and_Juliet_french.txt
+python3 main.py -i literary_works/Romeo_and_Juliet_ducth.txt -o processed_files/Romeo_and_Juliet_ducth.txt -r Romeo_and_Juliet_ducth.txt
+python3 main.py -i literary_works/Romeo_and_Juliet_english.txt -o processed_files/Romeo_and_Juliet_english.txt -r Romeo_and_Juliet_english.txt
+python3 main.py -i literary_works/Romeo_and_Juliet_german.txt -o processed_files/Romeo_and_Juliet_german.txt -r Romeo_and_Juliet_german.txt
+python3 main.py -i literary_works/Nazareth.txt -o processed_files/Nazareth.txt -r Nazareth.txt
+
+# Run the graphics.py script
+python3 graphics.py
